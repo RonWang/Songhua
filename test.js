82a60027e354f3d7fc8569f60cbcb5d673ba8063
RonWang <nongwang@amazon.com>
2019-11-05 09:26:57 -0800

    feat: change to use standard-version and replace semantic-release as the pre-release step

--- test.js
+++ test.js
@@ -5,4 +5,8 @@ describe('class inspection', () => {
     it ('init the class as Songhua', () => {
         expect(new Songhua(null, null, null, { isObjectMode: false })).instanceOf(Songhua);
     });
+
+    it ('init the class as Songhua 2', () => {
+        expect(new Songhua(null, null, null, { isObjectMode: false })).instanceOf(Songhua);
+    });
 });
